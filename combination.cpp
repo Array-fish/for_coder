ca5ae6ed7f605a820d5fc54f2c11ecfb1ea04617
array_fish <uekusa_hideaki@yahoo.co.jp>
2019-11-19 22:28:03 +0900

    [fix] change variable of combination library

--- combination.cpp
+++ combination.cpp
@@ -1,25 +1,25 @@
 struct Combination {
 
-	vector<ll> fact, rfact;
+	vector<llong> fact, rfact;
 
-	Combination(ll sz) : fact(sz + 1), rfact(sz + 1) {
+	Combination(llong sz) : fact(sz + 1), rfact(sz + 1) {
 
 		fact[0] = 1;
-		for (ll i = 1; i < fact.size(); i++) {
+		for (llong i = 1; i < fact.size(); i++) {
 			fact[i] = fact[i - 1] * i % MOD;
 		}
 		//逆元
 		rfact[sz] = inv(fact[sz]);
-		for (ll i = sz - 1; i >= 0; i--) {
+		for (llong i = sz - 1; i >= 0; i--) {
 			rfact[i] = rfact[i + 1] * (i + 1) % MOD;
 		}
 	}
 
-	ll inv(ll x){
+	llong inv(llong x){
 		return pow(x, MOD - 2);
 	}
-	ll pow(ll x, ll n){
-		ll ret = 1;
+	llong pow(llong x, llong n){
+		llong ret = 1;
 		while (n > 0) {
 			if (n & 1) (ret *= x) %= MOD;
 			(x *= x) %= MOD;
@@ -27,17 +27,17 @@ struct Combination {
 		}
 		return (ret);
 	}
-	ll P(ll n, ll r){
+	llong P(llong n, llong r){
 		if (r < 0 || n < r) return (0);
 		return (fact[n] * rfact[n - r] % MOD);
 	}
 
-	ll C(ll p, ll q){
+	llong C(llong p, llong q){
 		if (q < 0 || p < q) return (0);
 		return (fact[p] * rfact[q] % MOD * rfact[p - q] % MOD);
 	}
 
-	ll H(ll n, ll r){
+	llong H(llong n, llong r){
 		if (n < 0 || r < 0) return (0);
 		return (r == 0 ? 1 : C(n + r - 1, r));
 	}
